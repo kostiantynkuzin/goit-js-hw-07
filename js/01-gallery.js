db8ac27ae5289a6d706cc17f7e4fe14d651e7aaf
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-12 21:30:14 +0300

    01-start

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const containerGallery = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCards(galleryItems);
+containerGallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery_item">
+        <a class= "gallery_link" href="${original}">
+            <img class= "gallery_image"
+            src="${preview} 
+            data-source="${original}" 
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+}
+// console.log(createGalleryCards(galleryItems));
+containerGallery.addEventListener("click", clickOnGalleryCards);
+
+function clickOnGalleryCards(event) {
+  //   event.preventDefault();
+  if (!event.target.classList.contains("gallery_image")) {
+    return;
+  }
+  console.log(event.target.dataset.data - source);
+}
